5b3782522795b9f4a52ff34759703872d9cf7445
Piquixel <thomas.fetu2004@gmail.com>
2025-06-27 11:38:22 +0200

    update 7 files and delete 1 file

---
+++ src/ts/main.ts
@@ -0,0 +1,57 @@
+const
+	colorThief = new ColorThief(),
+
+	root = document.querySelector(':root') as HTMLElement,
+	imagePlaceholder = new Image(),
+	trackTitle = document.querySelector( 'h2' ),
+	trackArtist = document.querySelector( 'p' ),
+	API = {
+		url: 'https://api.lanyard.rest/v1',
+		id: '383187323963047936'
+	};
+
+let currentTrack = sessionStorage['currentTrack'];
+
+if ( !trackArtist || !trackTitle ) throw new Error( 'Missing elements' );
+
+const fetchResponse: FetchUserFunction = async (userId) => {
+	try {
+		return await fetch(`${API.url}/users/${userId}`).then((res) =>
+			res.json()
+		);
+	} catch (err) {
+			console.error(err);
+	}
+}
+
+const getSpotifyActivity = async () => {
+	const {
+		data: { listening_to_spotify, spotify },
+	} = await fetchResponse(API.id);
+
+	if ( !listening_to_spotify || spotify.track_id === currentTrack ) return;
+
+	currentTrack = spotify.track_id;
+
+	imagePlaceholder.addEventListener('load', () => {
+		root?.style.setProperty(
+				'--dominant-color',
+				`rgb(${colorThief.getColor(imagePlaceholder).join(',')})`
+			);
+	});
+
+	imagePlaceholder.crossOrigin = 'Anonymous';
+	imagePlaceholder.src = spotify.album_art_url;
+
+	const coverImages: NodeListOf<HTMLImageElement> = document.querySelectorAll( 'img' );
+
+	for (const image of coverImages) {
+		image.src = spotify.album_art_url;
+	}
+
+	trackTitle.textContent = spotify.song;
+	trackArtist.textContent = spotify.artist;
+};
+
+getSpotifyActivity();
+setInterval(getSpotifyActivity, 1000);
